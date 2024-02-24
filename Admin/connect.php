58df1c9a333a42b5488060ecf09d5295a4cd4f85
KhoiNguyen-sudo <hodangkhoinguyen02@gmail.com>
2024-02-24 15:18:55 +0700

    Sample commit

--- Admin/connect.php
+++ Admin/connect.php
@@ -1,11 +1,13 @@
 <?php
-//database có tên là test_db
-$sname = "us-cdbr-east-06.cleardb.net";
-$unmae = "bbcbe2106a19d4";
-$password = "c2bb67ae";
-$db_name = "heroku_445958caf28b0de";
+
+$sname= "localhost";
+$unmae= "root";
+$password = "";
+
+$db_name = "test_db";
+
 $conn = mysqli_connect($sname, $unmae, $password, $db_name);
+
 if (!$conn) {
-    echo "Kết nối thất bại";
+	echo "Connection failed!";
 }
-?>
